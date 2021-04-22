b8f4deb2ab201e2e88747343f5d09a8e3f59861b
unknown <DenilsonMelo>
2021-04-22 11:29:58 -0300

    adding player functionality

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,42 @@
 import '../styles/global.scss'
 
+import { useState } from 'react';
+
 import { Header } from "../components/Header/index";
 import { Player } from "../components/Player/index";
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from '../styles/app.module.scss'
 
 function MyApp({ Component, pageProps }) {
-  return(
-    <div className={styles.wrapper}>
-      
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      
-      <Player />
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  
+  function play(episode){
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
 
-    </div>
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+  
+  function setPlayingState(state: boolean){
+    setIsPlaying(state);
+  }
+
+  return(
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   ); 
 }
 
